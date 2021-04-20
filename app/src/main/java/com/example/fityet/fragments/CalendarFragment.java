d494a35a052b0ece1c06da0beb9127d208ae5678
lilysashad <lilysdeng@gmail.com>
2021-04-20 14:40:52 -0400

    Registration activity
    Registration activity/layout, fixing Android Manifest, more fragments with respective layouts

--- app/src/main/java/com/example/fityet/fragments/CalendarFragment.java
+++ app/src/main/java/com/example/fityet/fragments/CalendarFragment.java
@@ -3,33 +3,32 @@ package com.example.fityet.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import android.widget.Button;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.example.fityet.LoginActivity;
-import android.content.Intent;
-import com.parse.ParseUser;
+
 import com.example.fityet.R;
 
 /**
  * A simple {@link Fragment} subclass.
+ * Use the {@link CalendarFragment#newInstance} factory method to
+ * create an instance of this fragment.
  */
-
-public class ProfileFragment extends Fragment {
+public class CalendarFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private Button btnLogOut;
 
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
+    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    public CalendarFragment() {
+        // Required empty public constructor
+    }
 
     /**
      * Use this factory method to create a new instance of
@@ -37,12 +36,11 @@ public class ProfileFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PostsFragment.
+     * @return A new instance of fragment CalendarFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-
-        ProfileFragment fragment = new ProfileFragment();
+    public static CalendarFragment newInstance(String param1, String param2) {
+        CalendarFragment fragment = new CalendarFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,29 +60,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        // Inflate layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
-
-        btnLogOut = view.findViewById(R.id.btnLogOut);
-
-        btnLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                ParseUser.logOut();
-
-                Intent i = new Intent(getContext(), LoginActivity.class);
-                startActivity(i);
-
-            }
-        });
-
-
-    }
-
 }
