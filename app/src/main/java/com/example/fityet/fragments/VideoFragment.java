01ff0eb2d3593c380f77a26e77e9f98380a52d0f
Fariha Rashid <fariha.rashid99@gmail.com>
2021-04-28 00:53:08 +0300

    ProfileFragment + RegisterActivity.java updated

--- app/src/main/java/com/example/fityet/fragments/VideoFragment.java
+++ app/src/main/java/com/example/fityet/fragments/VideoFragment.java
@@ -17,6 +17,7 @@ import com.example.fityet.Exercise;
 import com.example.fityet.Models.Video;
 import com.example.fityet.R;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -59,6 +60,8 @@ public class VideoFragment extends Fragment {
         fillExercises();
 
         //FARIHA, IF YOU MADE PROFILE FRAGMENT'S GETTERS/SETTERS RUN PROPERLY, FIX THIS CODE TOO
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        userGoalName.setText(currentUser.getString("goal"));
         //userGoalName.setText(User.KEY_GOAL);
 
 
